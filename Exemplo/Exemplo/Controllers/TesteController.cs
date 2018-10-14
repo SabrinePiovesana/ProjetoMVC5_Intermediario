0943b75fd3e8772147ba49aea0b2e72133664470
Sabrine Piovesana <sabry.piovesana@gmail.com>
2018-10-14 20:25:00 -0300

    Teste

---
+++ Exemplo/Exemplo/Controllers/TesteController.cs
@@ -0,0 +1,24 @@
+﻿using Exemplo.Models;
+using System.Linq;
+using System.Web.Mvc;
+
+namespace Exemplo.Controllers
+{
+    public class TesteController : Controller
+    {
+        private BancoContexto db = new BancoContexto();
+
+        // GET: Teste
+        public ActionResult Index()
+        {
+            return View(db.Alunos.ToList());
+        }
+
+        public ViewResult Details(int id)
+        {
+            Aluno aluno = db.Alunos.Find(id);
+
+            return View(aluno);
+        }
+    }
+}
